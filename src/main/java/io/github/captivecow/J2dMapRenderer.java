f47dc33a6a93a1edb5611152d5820b4e24d79e65
captivecow <captivecow.dev@gmail.com>
2025-03-01 22:21:22 -0600

    feat: setup frame/canvas and load size from properties file

--- src/main/java/io/github/captivecow/J2dMapRenderer.java
+++ src/main/java/io/github/captivecow/J2dMapRenderer.java
@@ -1,7 +1,10 @@
 package io.github.captivecow;
 
+import javax.swing.*;
+
 public class J2dMapRenderer {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ScreenView screen = new ScreenView();
+        SwingUtilities.invokeLater(screen);
     }
 }
