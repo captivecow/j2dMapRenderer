870128c2199e0ecbab216834c556d92ee904d453
captivecow <109059183+captivecow@users.noreply.github.com>
2025-03-01 22:36:48 -0600

    feat: setup game screen and logger/properties file (#1)
    * feat: setup frame/canvas and load size from properties file
    
    * feat: catch missing config file issue, set defaults
    
    * chore: update imports and run formatter
    
    * chore: fix import in main class
    
    * chore: update visibility of properties

---
+++ src/main/java/io/github/captivecow/ScreenView.java
@@ -0,0 +1,72 @@
+package io.github.captivecow;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
+public class ScreenView implements Runnable {
+
+    private final String DEFAULT_SCREEN_WIDTH = "800";
+    private final String DEFAULT_SCREEN_HEIGHT = "600";
+
+    private final Logger logger = LoggerFactory.getLogger(ScreenView.class);
+
+    private final Properties properties;
+    private final JFrame frame;
+    private final Canvas canvas;
+    private final GridBagLayout layout;
+    private final GridBagConstraints constraints;
+
+    public ScreenView() {
+        properties = new Properties();
+        frame = new JFrame("j2dMapRenderer");
+        canvas = new Canvas();
+        layout = new GridBagLayout();
+        constraints = new GridBagConstraints();
+    }
+
+    public void createAndShowGui() {
+
+        try {
+            InputStream configPropertiesStream = Objects
+                    .requireNonNull(J2dMapRenderer.class.getResourceAsStream("/config.properties"));
+            properties.load(configPropertiesStream);
+        } catch (IOException | NullPointerException ex) {
+            logger.warn("Missing config file, using default width/height for screen.");
+        }
+
+        int screenWidth = Integer.parseInt(properties.getProperty("screen.width", DEFAULT_SCREEN_WIDTH));
+        int screenHeight = Integer.parseInt(properties.getProperty("screen.height", DEFAULT_SCREEN_HEIGHT));
+
+        canvas.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        canvas.setIgnoreRepaint(true);
+
+        frame.setLayout(layout);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        frame.add(canvas, constraints);
+
+        frame.setIgnoreRepaint(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void run() {
+        createAndShowGui();
+    }
+}
