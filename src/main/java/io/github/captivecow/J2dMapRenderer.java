de180fccb6eec5112a61d80c8399896efe4bdc5f
captivecow <109059183+captivecow@users.noreply.github.com>
2025-03-15 17:24:27 -0500

    feat: add a render loop and fps counter to screen (#5)
    * feat: added controller and render loop/delta to screen
    
    * feat: add default background on frame

--- src/main/java/io/github/captivecow/J2dMapRenderer.java
+++ src/main/java/io/github/captivecow/J2dMapRenderer.java
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities;
 
 public class J2dMapRenderer {
     public static void main(String[] args) {
-        ScreenView screen = new ScreenView();
-        SwingUtilities.invokeLater(screen);
+        ScreenController controller = new ScreenController();
+        controller.start();
     }
 }
---
+++ src/main/java/io/github/captivecow/ScreenController.java
@@ -0,0 +1,14 @@
+package io.github.captivecow;
+
+import javax.swing.SwingUtilities;
+public class ScreenController {
+    private final ScreenView screen;
+
+    public ScreenController(){
+        screen = new ScreenView();
+    }
+
+    public void start(){
+        SwingUtilities.invokeLater(screen);
+    }
+}
